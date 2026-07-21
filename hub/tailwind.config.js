d4a6e92b716333e681572a65d48a2caa2d6f18e8
James Hyatt <jameshyatt@my.tridenttech.edu>
2026-07-21 09:34:45 -0400

    style: upgrade typography to cinematic sci-fi fonts

--- hub/tailwind.config.js
+++ hub/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space Grotesk', 'sans-serif'],
+        heading: ['Rajdhani', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      }
+    },
   },
   plugins: [],
 }
